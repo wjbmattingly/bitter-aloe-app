b8ecdc18f444ef1038610204d84d1b2942996003
William Mattingly <62964060+wjbmattingly@users.noreply.github.com>
2022-06-10 15:22:32 -0400

    fixed database app
    Fixed an issue with the database app where it couldn't find multiple terms

--- pages/Volume 7 Database.py
+++ pages/Volume 7 Database.py
@@ -31,11 +31,11 @@ if form_options.form_submit_button():
         cond1 = df['descriptions'].str.findall('|'.join(description_search)).map(lambda x: len(set(x)) == len(description_search))
 
         new_df = df.loc[cond1, :]
-        st.write(len(new_df))
+        st.write(f"The Total Number of Results is {len(new_df)}")
         st.table(new_df)
     else:
         if len(description_search) > 0:
-            main_cond = df['descriptions'].str.fullmatch(description_search)
+            main_cond = df['descriptions'].apply(lambda x: any([k in x for k in description_search.split("|")]))
             if len(people_select) > 0:
                 cond2  = [x for x in df['names'].str.contains("|".join(people_select))]
                 i=0
@@ -51,34 +51,3 @@ if form_options.form_submit_button():
         new_df = df.loc[main_cond, :]
         st.write(f"The Total Number of Results is {len(new_df)}")
         st.table(new_df)
-# if strict_box == False:
-#     cond1  = [any(y in mss_select for y in x ) for x in df['valid_mss'].str.split(", ")]
-#     main_cond = cond1
-#     if len(people_select) > 0:
-#         cond2  = [x for x in df['pase_refs'].str.contains("|".join(people_select))]
-#         main_cond = cond1
-#         i=0
-#         for c in cond2:
-#             if c == True:
-#                 main_cond[i] = True
-#             i=i+1
-#
-#     new_df = df.loc[main_cond, :]
-# else:
-#     #https://stackoverflow.com/questions/60932036/check-if-pandas-column-contains-all-elements-from-a-list
-#     mss_select = set(mss_select)
-#     cond1 = [x for x in df['valid_mss'].str.split(", ").apply(lambda x: set(mss_select).issubset(x))]
-#     cond2 = [x for x in df['pase_refs'].apply(lambda x: np.all([*map(lambda l: l in x, people_select)]))]
-#
-#
-#     main_cond = []
-#     i=0
-#     for c in cond2:
-#         if c == True and cond1[i] == True:
-#             main_cond.append(True)
-#         else:
-#             main_cond.append(False)
-#         i=i+1
-#     new_df = df.loc[main_cond, :]
-#
-# st.table(new_df)
